e69f6b3c2d38e02d58d9199b1ec9a248c01e8f6f
elsujeto866 <doriandavidmza@gmail.com>
2025-12-09 16:36:50 -0500

    feat(domain): define ClientRepositoryPort output interface

---
+++ clients/src/main/java/com/banking/clients/domain/ports/input/ClientRepositoryPort.java
@@ -0,0 +1,17 @@
+package com.banking.clients.domain.ports.input;
+
+import com.banking.clients.domain.model.Client;
+import com.banking.clients.domain.model.vo.Identification;
+import com.banking.clients.domain.model.vo.PersonId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientRepositoryPort {
+    Client save(Client client);
+    Optional<Client> findById(PersonId id);
+    Optional<Client> findByIdentification(Identification identification);
+    List<Client> findAll();
+    boolean existsByIdentification(Identification identification);
+    void deleteById(PersonId id);
+}
